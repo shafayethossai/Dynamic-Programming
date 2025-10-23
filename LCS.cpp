2c7c5575a2ac0fe60765b7ed7bab6ac994717490
shafayethossai <khansh921@gmail.com>
2025-10-24 01:48:25 +0600

    new

---
+++ LCS.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+const int N = 3000;
+string a, b;
+int dp[N][N];
+
+int lcs (int i, int j) {
+    if (i >= a.size() || j >= b.size()) {
+        return 0;
+    }
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
+    int ans = lcs(i+1, j);
+    ans = max(ans, lcs(i, j+1));
+
+    if (a[i] == b[j]) {
+        ans = max(ans, lcs(i+1, j+1) + 1);
+    }
+    return dp[i][j] = ans;
+}
+
+void print (int i, int j) {
+    if (i >= a.size() || j >= b.size()) {
+        return;
+    }
+    if (a[i] == b[j]) {
+        cout << a[i];
+
+        print(i+1, j+1);
+        return;
+    }
+    int x = lcs(i+1, j);
+    int y = lcs(i, j+1);
+
+    if (x >= y) {
+        print(i+1, j);
+    }
+    else {
+        print(i, j+1);
+    }
+}
+
+int main() {
+    fast;
+    
+    cin >> a >> b;
+    memset(dp, -1, sizeof dp);
+
+    cout << lcs(0, 0) << endl;
+    print(0, 0);
+
+    return 0;
+}
